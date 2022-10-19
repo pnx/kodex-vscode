f15f6589e1528961def614c6e95b8da98faa678c
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2022-10-19 18:40:03 +0200

    src/build.ts: build themes defined in package.json

--- src/build.ts
+++ src/build.ts
@@ -1,16 +1,16 @@
+import config from '../package.json'
 import * as fs from 'fs'
 import * as path from 'path'
 import themes from './themes'
 import template from './template'
 
-const filePath = (name: string) : string =>
-	path.join(process.cwd(), `/themes/${name}.json`)
-
-const generate = (name: string, filename: string, scheme: any) => {
-	const path = filePath(filename)
-	const content = JSON.stringify(template(name, scheme), null, 4)
-	fs.writeFileSync(path, content)
+const generate = (name: string, filename: string, theme: any) => {
+	const file = path.normalize(path.join(process.cwd(), filename))
+	const content = JSON.stringify(template(name, theme), null, 4)
+	fs.writeFileSync(file, content)
 	console.log(`Generated ${name}!`)
 }
 
-generate('Kodex', 'Kodex-color-theme', themes.default)
+config.contributes.themes.forEach(item => {
+    generate(item.label, item.path, themes[item.theme])
+});
