2299e2b397461fd9529e46aca7c71a7973d017c8
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2023-06-09 16:58:33 +0200

    src/template.ts: Adding keyword.operator as keyword

--- src/template.ts
+++ src/template.ts
@@ -383,6 +383,7 @@ export default (name: string, scheme: any) => {
             {
                 "name": "Keyword, Namespace",
                 "scope": [
+                    "keyword.operator",
                     "keyword.control",
                     "keyword.other",
                     "keyword.cmake",
